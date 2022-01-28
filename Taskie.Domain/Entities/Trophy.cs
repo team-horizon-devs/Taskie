11aca4dfa9ea942ee01ced05c3af7404d78a4c1c
Ricsoon <painonex2@gmail.com>
2022-01-28 00:12:40 -0300

    Criação das entidades de trofẽis e troféis de usuários

---
+++ Taskie.Domain/Entities/Trophy.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace Taskie.Domain.Entities
+{
+    class Trophy : BaseEntity
+
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int PricePoints { get; set; }
+    }
+}
---
+++ Taskie.Domain/Entities/TrophyUser.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace Taskie.Domain.Entities
+{
+    class TrophyUser : BaseEntity
+    {
+        public string UserId { get; set; }
+        public int TrophyId { get; set; }
+        public Trophy Trophy { get; set; }
+        public User User { get; set; }
+    }
+}
