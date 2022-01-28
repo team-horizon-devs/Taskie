dd54d5c02e3853f8d9e875ed6ebe0d91976f4ded
dolglasmesquita <dolglas_mesquita@hotmail.com>
2022-01-28 01:17:56 -0300

    Criada a classe TaskieContext

--- Taskie.Domain/Entities/AchievementUser.cs
+++ Taskie.Domain/Entities/AchievementUser.cs
@@ -6,7 +6,7 @@ namespace Taskie.Domain.Entities
     {
         public string UserId { get; set; }
         public User User { get; set; }
-        public int AchievementsId { get; set; }
+        public int AchievementId { get; set; }
         public Achievement Achievement { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
--- Taskie.Infra.Data/Context/TaskieContext.cs
+++ Taskie.Infra.Data/Context/TaskieContext.cs
@@ -1,10 +1,34 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Taskie.Domain.Entities;
 
 namespace Taskie.Infra.Data.Context
 {
     class TaskieContext : IdentityDbContext<User>
     {
+        public DbSet<Achievement> Achievements { get; set; }
+        public DbSet<AchievementUser> AchievementsUsers { get; set; }
+        public DbSet<Avatar> Avatars { get; set; }
+        public DbSet<FinishedInTime> FinishedsInTime { get; set; }
+        public DbSet<Task> Tasks { get; set; }
+        public DbSet<Trophy> Trophies { get; set; }
+        public DbSet<TrophyUser> TrophiesUsers { get; set; }
+        public DbSet<UserPoint> UsersPoints { get; set; }
+
+        public TaskieContext(DbContextOptions<TaskieContext> options) : base(options)
+        {
+                
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<TrophyUser>().HasKey(tu => new {tu.UserId, tu.TrophyId});
+            builder.Entity<AchievementUser>().HasKey(au => new { au.UserId, au.AchievementId });
+            
+        }
+
+
+
 
     }
 }
