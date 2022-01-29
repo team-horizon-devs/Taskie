88af1f0d39a9c74afe286062c53626193517bba9
dolglasmesquita <dolglas_mesquita@hotmail.com>
2022-01-29 00:03:56 -0300

    Criado IRepository e implementado a BaseRepository

---
+++ Taskie.Infra.Data/Repository/BaseRepository.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using Taskie.Domain.Interfaces.Repository;
+using Taskie.Infra.Data.Context;
+
+namespace Taskie.Infra.Data.Repository
+{
+    public class BaseRepository<T> : IRepository<T> where T : class
+    {
+        private readonly TaskieContext _context;
+        private readonly DbSet<T> _dataSet;
+
+        public BaseRepository(TaskieContext context)
+        {
+            _context = context;
+            _dataSet = _context.Set<T>();
+        }
+
+        public void Create(T obj)
+        {
+            _context.Add(obj);
+            _context.SaveChanges();
+        }
+
+        public void Delete(T obj)
+        {
+            _context.Remove(obj);
+            _context.SaveChanges();
+
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _dataSet.ToArray();
+        }
+
+        public void Update(T obj)
+        {
+            _context.Update(obj);
+            _context.SaveChanges();
+        }
+
+    }
+}
