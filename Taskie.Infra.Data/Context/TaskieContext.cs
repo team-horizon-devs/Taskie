e176f957b4a965b5f0b3310863bebf8f4284dea8
dolglasmesquita <dolglas_mesquita@hotmail.com>
2022-02-02 01:31:33 -0300

    Removida entidade FinishedInTime e gerada nova migration

--- Taskie.Infra.Data/Context/TaskieContext.cs
+++ Taskie.Infra.Data/Context/TaskieContext.cs
@@ -9,7 +9,6 @@ namespace Taskie.Infra.Data.Context
         public DbSet<Achievement> Achievements { get; set; }
         public DbSet<AchievementUser> AchievementsUsers { get; set; }
         public DbSet<Avatar> Avatars { get; set; }
-        public DbSet<FinishedInTime> FinishedsInTime { get; set; }
         public DbSet<Task> Tasks { get; set; }
         public DbSet<Trophy> Trophies { get; set; }
         public DbSet<TrophyUser> TrophiesUsers { get; set; }
@@ -27,8 +26,6 @@ namespace Taskie.Infra.Data.Context
             builder.Entity<TrophyUser>().HasKey(tu => new {tu.UserId, tu.TrophyId});
             builder.Entity<AchievementUser>().HasKey(au => new { au.UserId, au.AchievementId });
             builder.Entity<UserPoint>().HasKey(up => new { up.UserId});
-            builder.Entity<FinishedInTime>().HasKey(ft => new { ft.UserId });
-
         }
 
     }
