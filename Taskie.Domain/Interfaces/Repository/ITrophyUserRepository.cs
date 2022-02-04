9d073cc14b62ea43a3c1750f98676ea94b1be5f0
dolglasmesquita <dolglas_mesquita@hotmail.com>
2022-02-04 17:07:31 -0300

    adicionado interface e implementação de trophyuser

---
+++ Taskie.Domain/Interfaces/Repository/ITrophyUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Threading.Tasks;
+using Taskie.Domain.Entities;
+
+namespace Taskie.Domain.Interfaces.Repository
+{
+    public interface ITrophyUserRepository
+    {
+        Task<TrophyUserEntity> CreateAsync(TrophyUserEntity trophyEntity);
+        Task<IEnumerable<TrophyUserEntity>> GetAllTrophiesByUserIdAsync(string idUser);
+
+    }
+}
---
+++ Taskie.Infra.Data/Repository/TrophyUserRepository.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Taskie.Domain.Entities;
+using Taskie.Domain.Interfaces.Repository;
+using Taskie.Infra.Data.Context;
+
+namespace Taskie.Infra.Data.Repository
+{
+    public class TrophyUserRepository : ITrophyUserRepository
+    {
+        private readonly TaskieContext _context;
+
+        public TrophyUserRepository(TaskieContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<TrophyUserEntity> CreateAsync(TrophyUserEntity trophyEntity)
+        {
+            _context.TrophiesUsers.Add(trophyEntity);
+            await _context.SaveChangesAsync();
+
+            return trophyEntity;
+        }
+
+        public async Task<IEnumerable<TrophyUserEntity>> GetAllTrophiesByUserIdAsync(string idUser)
+        {
+            IQueryable<TrophyUserEntity> query = _context.TrophiesUsers;
+            query = query.Include(tu => tu.Tropy).Where(tu => tu.UserId == idUser);
+
+            return await query.AsNoTracking().ToListAsync();
+        }
+       
+    }
+}
