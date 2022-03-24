d760305618446a5a8cbe6e9861e0afa6a4b20f13
dolglasmesquita <dolglas_mesquita@hotmail.com>
2022-03-24 17:10:53 -0300

    Ajuste user entity

--- Taskie.Domain/Entities/UserEntity.cs
+++ Taskie.Domain/Entities/UserEntity.cs
@@ -1,16 +1,23 @@
-﻿using Microsoft.AspNetCore.Identity;
-using System;
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Taskie.Domain.Entities
 {
     public class UserEntity : IdentityUser
     {
+        [Required]
+        [MaxLength(80)]
+        public string Name { get; set; }
         public bool Active { get; set; } = true;
-        public DateTime LastLogin { get; set; }
-        public int AvatarId { get; set; }
+        public int AvatarId { get; set; } = 1;
         public AvatarEntity Avatar { get; set; }
+        public int Point { get; set; } = 0;
+        public IEnumerable<TrophyUserEntity> TrophiesUser { get; set; }
+        public IEnumerable<AchievementUserEntity> AchievementsUser { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.Parse(DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss"));
-        public DateTime? UpdatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; } = null;
 
     }
 }
